5962c7992b7096ab94fcbd83e5adc07368ec700f
satya <satya143@gmail.com>
2023-02-19 14:42:48 +0530

    Added:Search a node which value is 30 in linkedList

--- src/linkedListSolution/LinkedList.java
+++ src/linkedListSolution/LinkedList.java
@@ -1,84 +1,49 @@
 package linkedListSolution;
 
-import java.util.*;
 import java.lang.*;
-import java.io.*;
 
 class LinkedList {
-        Node head; // head of list
+    Node head;
 
-        /* Linked list Node*/
-        class Node {
-            int data;
-            Node next;
-            Node(int d)
-            {
-                data = d;
-                next = null;
-            }
-        }
+    class Node {
+        int data;
+        Node next;
 
-        /* function to insert a
-    new_node in a list. */
-        void sortedInsert(Node new_node)
-        {
-            Node current;
-
-            /* Special case for head node */
-            if (head == null || head.data
-                    >= new_node.data) {
-                new_node.next = head;
-                head = new_node;
-            }
-            else {
-
-                /* Locate the node before point of insertion. */
-                current = head;
-
-                while (current.next != null
-                        && current.next.data < new_node.data) {
-
-                    current = current.next;
-                }
-
-                new_node.next = current.next;
-                current.next = new_node;
-            }
-        }
-
-        /*Utility functions*/
-
-        /* Function to create a node */
-        Node newNode(int data)
-        {
-            Node x = new Node(data);
-            return x;
-        }
-
-        /* Function to print linked list */
-        void printList()
-        {
-            Node temp = head;
-            while (temp != null) {
-                System.out.print(temp.data + " ");
-                temp = temp.next;
-            }
-        }
-
-        /* Driver function to test above methods */
-        public static void main(String args[])
-        {
-            LinkedList llist = new LinkedList();
-            Node new_node;
-            new_node = llist.newNode(56);
-            llist.sortedInsert(new_node);
-            new_node = llist.newNode(30);
-            llist.sortedInsert(new_node);
-            new_node = llist.newNode(40);
-            llist.sortedInsert(new_node);
-            new_node = llist.newNode(70);
-            llist.sortedInsert(new_node);
-            System.out.println("sorted Linked List");
-            llist.printList();
+        Node(int d) {
+            data = d;
+            next = null;
         }
     }
+
+    public void push(int new_data) {
+        Node new_node = new Node(new_data);
+        new_node.next = head;
+        head = new_node;
+    }
+
+    public boolean search(Node head, int x) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == x)
+                return true;
+            current = current.next;
+        }
+        return false;
+    }
+
+    public static void main(String args[]) {
+        LinkedList llist = new LinkedList();
+
+        llist.push(56);
+        llist.push(30);
+        llist.push(40);
+        llist.push(70);
+
+        if (llist.search(llist.head, 30))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+    }
+}
+
+
